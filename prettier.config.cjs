0f2c2efb27db52179c559efb7b92c76d53fb76da
bebob2 <konto2228@gmail.com>
2023-09-17 17:10:55 +0200

    template

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+
---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { cn } from "./utils/class-names"
+
+type Props = {}
+
+export const App = (props: Props) => {
+  return (
+    <div className={cn('bg-blue-950','bg-gray-900')}>app</div>
+  )
+}
